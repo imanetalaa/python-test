ab33d35b7d2a259456170d1832624c86f957d7f7
imanetalaa <imane.talaa@gmail.com>
2024-10-15 19:36:30 +0200

    Create test_example.py

---
+++ test_example.py
@@ -0,0 +1,7 @@
+# test_example.py
+
+def test_example(files):
+    file1, file2 = files
+    # Implémentez votre logique de test avec file1 et file2
+    assert file1 is not None
+    assert file2 is not None
