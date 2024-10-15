3cc46f15c8e0282c54dc00ed2d7a1ba3834419f6
imanetalaa <imane.talaa@gmail.com>
2024-10-15 19:48:21 +0200

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -1,18 +1,21 @@
 # test_compare_files.py
 import pytest
+import os
 
 def read_file(file_path):
     """Lit le contenu d'un fichier et renvoie une liste de lignes."""
     with open(file_path, 'r') as f:
         return f.readlines()
 
-def test_file_comparison(FILE1, FILE2):
+def test_file_comparison():
     """Compare le contenu de file1 et file2."""
+    # Obtenez les fichiers à partir des variables d'environnement
+    file1 = os.environ.get('FILE1')
+    file2 = os.environ.get('FILE2')
+
     # Lire le contenu des fichiers
-    content_file1 = read_file(FILE1)
-    content_file2 = read_file(FILE2)
+    content_file1 = read_file(file1)
+    content_file2 = read_file(file2)
 
     # Vérifier que le contenu des fichiers est identique
-    assert content_file1 == content_file2, f"Les fichiers {FILE1} et {FILE2} sont différents."
-
-# Note: Vous pouvez passer les paramètres FILE1 et FILE2 à pytest lors de l'exécution
+    assert content_file1 == content_file2, f"Les fichiers {file1} et {file2} sont différents."
